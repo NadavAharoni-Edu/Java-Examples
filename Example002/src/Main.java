7be63de4b0e415e377dc04f3180e5c9bce4daf10
Nadav Aharoni <nadav.aharoni.michlala@gmail.com>
2025-06-29 00:11:25 +0300

    Removed printing loop.
    It was from the auto-generated example, not needed any more,

--- Example002/src/Main.java
+++ Example002/src/Main.java
@@ -13,12 +13,6 @@ public class Main {
         MyInteger j2 = new MyInteger(8);
         increment(j2);
         System.out.println("j2 = " + j2.intValue());
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 
     public static void increment(int i)
