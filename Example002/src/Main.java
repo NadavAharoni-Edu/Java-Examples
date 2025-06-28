88fd6bbb37d3c9f7f0727bf32fe144997a98a772
Nadav Aharoni <nadav.aharoni.michlala@gmail.com>
2025-06-28 23:25:16 +0300

    A second IntelliJ project, increment func
    Created a second IntelliJ project and added two increment functions, one for a primitive integer and one for a class containing an integer.

---
+++ Example002/src/Main.java
@@ -0,0 +1,39 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+        int j=3;
+        increment(j);
+        System.out.println("j = " + j);
+
+        MyInteger j2 = new MyInteger();
+        j2.i = 8;
+        increment(j2);
+        System.out.println("j2 = " + j2.i);
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+
+    public static void increment(int i)
+    {
+        i++;
+    }
+
+    public static void increment(MyInteger i)
+    {
+        i.i++;
+    }
+
+}
+
+class MyInteger {
+    public int i;
+}
